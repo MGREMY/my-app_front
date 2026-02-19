7b4a13ac23dc55ed60ab0bbfd5a511e9257834f7
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-02-19 19:14:40 +0100

    fix: minor fixes

--- apps/my-app/src/app/core/config/app.config.ts
+++ apps/my-app/src/app/core/config/app.config.ts
@@ -1,13 +1,13 @@
 import { APP_CONFIG_SERVICE } from '@my-app/core/app-config.service';
 import { AppConfigService } from '@my-app/core/services/app-config.service';
 
-import { APP_BASE_HREF, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { APP_BASE_HREF, HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { inject, Provider } from '@angular/core';
 
 export function provideApplicationConfig(): Provider[] {
   return [
     { provide: APP_CONFIG_SERVICE, useClass: AppConfigService },
-    { provide: LocationStrategy, useClass: PathLocationStrategy },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     { provide: APP_BASE_HREF, useFactory: () => inject(APP_CONFIG_SERVICE).appBaseHref },
   ];
 }
