7fafc0b9dc5d58796abf8077d9493f46589ea164
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-03-15 01:13:10 +0100

    chore: change base api

--- apps/my-app/src/environments/environment.development.ts
+++ apps/my-app/src/environments/environment.development.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   production: false,
   appUrl: 'http://localhost:4200',
   appBaseHref: '/',
-  apiUrl: 'http://localhost:5230',
+  apiUrl: 'http://localhost:5230/api',
   defaultLanguage: 'fr-FR',
   authUrl: 'http://localhost:8080',
   authRealm: 'myApp',
