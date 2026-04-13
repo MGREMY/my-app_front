6d2b7f23d208dda8df735549b1200cf2fb10b869
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-04-13 19:29:25 +0200

    chore: update api uri in prod

--- apps/my-app/src/environments/environment.production.ts
+++ apps/my-app/src/environments/environment.production.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   production: true,
   appUrl: 'http://localhost:4200',
   appBaseHref: '/',
-  apiUrl: 'http://localhost:5230',
+  apiUrl: 'http://localhost:5230/api',
   defaultLanguage: 'fr-FR',
   authUrl: '',
   authRealm: '',
