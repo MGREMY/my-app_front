d9ba39f81f46b821895767410a2c0a57637d01cf
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-05-11 19:05:33 +0200

    feat(user): change detail template

--- apps/my-app/src/app/features/user/user-detail/user-detail.component.ts
+++ apps/my-app/src/app/features/user/user-detail/user-detail.component.ts
@@ -1,6 +1,21 @@
+import { AuthService } from '@/core/api/auth/auth.service';
 import { UserResponse } from '@/core/api/user/user.response';
 import { LocalizedDatePipe } from '@/shared/pipes/date.pipe';
 
+import {
+  MgnpAccordion,
+  MgnpAccordionContent,
+  MgnpAccordionItem,
+} from '@mgremy/ng-primitives/accordion';
+import { MgnpButton } from '@mgremy/ng-primitives/button';
+import { MgnpCheckbox } from '@mgremy/ng-primitives/checkbox';
+import {
+  NgpAccordion,
+  NgpAccordionContent,
+  NgpAccordionItem,
+  NgpAccordionTrigger,
+} from 'ng-primitives/accordion';
+import { NgpButton } from 'ng-primitives/button';
 import { NgpCheckbox } from 'ng-primitives/checkbox';
 
 import { NgIcon, provideIcons } from '@ng-icons/core';
@@ -8,11 +23,33 @@ import { heroCheckMini } from '@ng-icons/heroicons/mini';
 import { TranslatePipe } from '@ngx-translate/core';
 
 import { TitleCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, model, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  model,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [NgpCheckbox, NgIcon, LocalizedDatePipe, TranslatePipe, TitleCasePipe],
+  imports: [
+    MgnpButton,
+    MgnpCheckbox,
+    MgnpAccordion,
+    MgnpAccordionItem,
+    MgnpAccordionContent,
+    NgpButton,
+    NgpCheckbox,
+    NgpAccordion,
+    NgpAccordionItem,
+    NgpAccordionContent,
+    NgpAccordionTrigger,
+    NgIcon,
+    LocalizedDatePipe,
+    TranslatePipe,
+    TitleCasePipe,
+  ],
   templateUrl: './user-detail.component.html',
   providers: [
     provideIcons({
@@ -23,5 +60,8 @@ import { ChangeDetectionStrategy, Component, model, ViewEncapsulation } from '@a
   encapsulation: ViewEncapsulation.None,
 })
 export class UserDetailComponent {
+  protected readonly _authService = inject(AuthService);
+
   public readonly user = model.required<UserResponse>();
+  public readonly current = model<boolean>(false);
 }
